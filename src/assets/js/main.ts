8c2cbe300cb9f8f7e790eb2cad40c95991c45ab5
IQBAL HASSAN <iqbal.hassan2584@gmail.com>
2025-03-01 09:03:23 +0500

    fixs

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,1 +1,31 @@
-import "../css/main.css";
+import '../css/main.css';
+import Swiper from 'swiper/bundle';
+
+document.getElementById('menuBtn')!.addEventListener('click', function () {
+  document.getElementById('mobileMenu')!.classList.toggle('hidden');
+});
+
+document.getElementById('langBtn')!.addEventListener('click', function () {
+  document.getElementById('langDropdown')!.classList.toggle('hidden');
+});
+
+document.getElementById('mobileLangBtn')!.addEventListener('click', function () {
+  document.getElementById('mobileLangDropdown')!.classList.toggle('hidden');
+});
+
+//  Telco partners slider
+new Swiper('#telco_partner', {
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.telco_partner_button_next',
+    prevEl: '.telco_partner_button_prev'
+  },
+  breakpoints: {
+    320: { slidesPerView: 2 },
+    480: { slidesPerView: 2 },
+    640: { slidesPerView: 3 },
+    768: { slidesPerView: 4 },
+    1024: { slidesPerView: 5 }
+  }
+});
