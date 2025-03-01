78270f85a32dec7254bc2882ebc91c5b2d9a2ac5
IQBAL HASSAN <iqbal.hassan2584@gmail.com>
2025-03-01 14:52:59 +0500

    feature slider complete

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,5 +1,5 @@
 import '../css/main.css';
-// import Swiper from 'swiper/bundle';
+import Swiper from 'swiper/bundle';
 
 document.getElementById('menuBtn')!.addEventListener('click', function () {
   document.getElementById('mobileMenu')!.classList.toggle('hidden');
@@ -29,3 +29,24 @@ document.getElementById('mobileLangBtn')!.addEventListener('click', function () 
 //     1024: { slidesPerView: 5 }
 //   }
 // });
+new Swiper('#feature_slider', {
+  loop: true,
+  pagination: {
+    el: '.feature_slider_pagination',
+    clickable: true,
+    renderBullet: function (_index, className) {
+      return `<span class="${className} custom-bullet"></span>`;
+    }
+  },
+  navigation: {
+    nextEl: '#fs_btn_next',
+    prevEl: '#fs_btn_prev'
+  },
+  breakpoints: {
+    280: { pagination: { enabled: false } },
+    320: { slidesPerView: 2, spaceBetween: 10, pagination: { enabled: false } },
+    480: { slidesPerView: 2, spaceBetween: 10, pagination: { enabled: false } },
+    672: { slidesPerView: 2, spaceBetween: 10 },
+    896: { slidesPerView: 3, spaceBetween: 20 }
+  }
+});
